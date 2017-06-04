15d8be7b1a2bd8671d8e61a414cfc796ad9f13f9
dbqp9876 <dbqp9876@gmail.com>
2017-06-04 21:56:28 +0900

    修正回傳的JSON格式及功能

--- WebSocket/TestWebSocketHandler.cs
+++ WebSocket/TestWebSocketHandler.cs
@@ -5,63 +5,80 @@ using System.Threading;
 using System.Web;
 using Microsoft.Web.WebSockets;
 using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 
 public class TestWebSocketHandler : WebSocketHandler
 {
-  private static WebSocketCollection clients = new WebSocketCollection();
-  private string name;
-  private string room;
-  private JObject all_message = new JObject() { };
+    private static WebSocketCollection clients = new WebSocketCollection();
+    private string name;
+    private string room;
+    private JObject all_message = new JObject() { };
 
-  public TestWebSocketHandler()
-  {
-  }
-
-  public override void OnOpen()
-  {
-    this.name = this.WebSocketContext.QueryString["name"];
-    this.room = this.WebSocketContext.QueryString["room"];
-
-    clients.Add(this);
-    clients.Broadcast(name + " has connected.");
-  }
-
-  public override void OnMessage(string message)
-  {
-    clients.Broadcast("back onmessage");
-
-    setMessage(name, message);
-    //特定群組
-    /*var channel = clients.FirstOrDefault(n => ((TestWebSocketHandler)n).room == this.room);
-    if (channel != null)
+    public TestWebSocketHandler()
     {
-        JArray reData = new JArray();
-        reData.Add(new JArray() { "" });
-        channel.Send((string)reData);
-    }*/
+    }
 
-    clients.Broadcast((string)this.all_message);
-  }
+    public override void OnOpen()
+    {
+        this.name = this.WebSocketContext.QueryString["name"];//使用者名稱
+        this.room = this.WebSocketContext.QueryString["room"];//房間名稱
 
-  public override void OnClose()
-  {
-    clients.Remove(this);
-    clients.Broadcast(string.Format("{0} has gone away.", name));
-  }
+        clients.Add(this);
+        clients.Broadcast(name + " has connected.");
+    }
 
-  public override void OnError()
-  {
-    clients.Broadcast(string.Format("Error：{0}", base.Error.ToString()));
-  }
+    public override void OnMessage(string message)
+    {
+        clients.Broadcast("back onmessage");
+        try
+        {
+            setMessage(name, message);
+            string reJStr = JsonConvert.SerializeObject(all_message);
+            clients.Broadcast(reJStr);
+        }
+        catch (Exception ex)
+        {
+            clients.Broadcast(ex.ToString());
+        }
 
-  public void setMessage(string name, string message)
-  {
-    JArray tempArray = new JArray();
-    JArray talk = new JArray() { name, message };
-    tempArray.AddAfterSelf(talk);
+        //特定群組
+        /*var channel = clients.FirstOrDefault(n => ((TestWebSocketHandler)n).room == this.room);
+        if (channel != null)
+        {
+            JArray reData = new JArray();
+            reData.Add(new JArray() { "" });
+            channel.Send((string)reData);
+        }*/
+    }
 
-    all_message[this.room].AddAfterSelf(tempArray);
-  }
+    public override void OnClose()
+    {
+        clients.Remove(this);
+        clients.Broadcast(string.Format("{0} has gone away.", name));
+    }
+
+    public override void OnError()
+    {
+        clients.Broadcast(string.Format("Error：{0}", base.Error.ToString()));
+    }
+
+    public void setMessage(string name, string message)
+    {
+        JObject talk = new JObject() { new JProperty("name", name), new JProperty("message", message) };
+
+        if (all_message[this.room] == null)
+        {
+            JArray tempArray = new JArray();
+            tempArray.Add(talk);
+            JObject tempObject = new JObject() { new JProperty("talk", tempArray) };
+            all_message.Add(new JProperty(this.room, tempObject));
+        }
+        else
+        {
+            JArray temp = (JArray)all_message[this.room]["talk"];
+            //傳址
+            temp.Add(talk);
+        }
+    }
 
 }
-
